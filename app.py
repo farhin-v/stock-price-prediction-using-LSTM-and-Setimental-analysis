0ba6ca92e76ee3d7cdd046f7945cc2e198b84eec
farhin-v <fvasaya@leomail.tamuc.edu>
2026-05-08 21:51:13 +0530

    Add initial Gradio app setup

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+import gradio as gr
 
+def predict(stock):
+    return f"Stock prediction generated for {stock}"
+
+demo = gr.Interface(
+    fn=predict,
+    inputs="text",
+    outputs="text",
+    title="AI Stock Price Prediction System"
+)
+
+demo.launch()
